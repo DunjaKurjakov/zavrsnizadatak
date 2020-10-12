f163719be8208793b3283536a318cccaad950ffe
dunjakurjakov <dunjakurjakov@gmail.com>
2020-10-12 21:54:35 +0200

    postovi na home

--- posts.php
+++ posts.php
@@ -1,19 +1,42 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="shortcut icon" href="favicon.ico">
-    <title>Vivify Academy Blog - Homepage</title>
-
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body class="va-l-page va-l-page--homepage">
-
 <?php
-    include('header.php');
-    echo('all posts page');
-    include('footer.php');
+
+
+$sql = "SELECT id, title, body, author, created_at FROM posts ORDER BY created_at DESC " ;
+
+$statement = $connection->prepare($sql);
+
+$statement->execute();
+
+$statement->setFetchMode(PDO::FETCH_ASSOC);
+
+$posts = $statement->fetchAll();
+
+
 ?>
+
+    <div class="col-sm-8 blog-main">
+
+    <?php
+        foreach ($posts as $post) {
+    ?>
+
+        <article class="va-c-article">
+            <header>
+                <h1><a href="single-post.php?post_id=<?php echo($post['id']) ?>"><?php echo($post['title']) ?></a></h1>
+                <div class="va-c-article__meta"><?php echo($post['created_at']) ?> by <?php echo($post['author']) ?></div>
+            </header>
+
+            <div>
+                <p><?php echo($post['body']) ?></p>
+            </div>
+        </article>
+
+       <?php
+           }
+       ?>
+            <nav class="blog-pagination">
+                <a class="btn btn-outline-primary" href="#">Older</a>
+                <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+            </nav>
+
+        </div>
